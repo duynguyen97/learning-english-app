8fafc07ff4f48eb12ee2a7d91bf4479c2914389a
duynguyen97 <duymanh8683@gmail.com>
2023-02-23 17:51:09 +0700

    setup deploy

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,29 @@ const app = express();
 
 // set environment variables
 require("dotenv").config();
+
+// ================== setup ==================
+app.use(express.static(path.join(__dirname, "/src/build")));
+
+const dev = app.get("env") !== "production";
+
+if (!dev) {
+  app.disable("x-powered-by");
+  app.use(morgan("common"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/src/build", "index.html"));
+  });
+
+  // Auto wake up heroku
+  // app.get('/apis/wakeup-heroku', (req, res) => res.send('ok'));
+  // const timer = 25 * 60 * 1000; // 25 minutes
+  // setInterval(() => {
+  //   https.get('https://dynonary.herokuapp.com/apis/wakeup-heroku');
+  // }, timer);
+} else {
+  app.use(morgan("dev"));
+}
+
 // ================== Connect mongodb with mongoose ==================
 const mongoose = require("mongoose");
 const MONGO_URL = process.env.MONGO_URL;
