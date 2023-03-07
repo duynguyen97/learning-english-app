993299ab51d7cb95945bb3e83ecdcd5eb8893746
duynguyen97 <duymanh8683@gmail.com>
2023-03-07 10:28:14 +0700

    disable function processing

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -2,8 +2,8 @@ import Layout from 'components/Layout';
 import { ROUTES } from 'constant';
 import React from 'react';
 import flashcardIcon from 'assets/icons/flashcard.png';
-import dictionaryIcon from 'assets/icons/dictionary.png';
-import favoriteIcon from 'assets/icons/favorite.png';
+// import dictionaryIcon from 'assets/icons/dictionary.png';
+// import favoriteIcon from 'assets/icons/favorite.png';
 import editIcon from 'assets/icons/edit.png';
 import { Grid } from '@mui/material';
 import FeatureBox from 'components/FeatureBox';
@@ -16,16 +16,16 @@ const HomePage = () => {
       imgUrl: flashcardIcon,
       to: ROUTES.FLASHCARD,
     },
-    {
-      title: 'Từ điển trong EasyEnglish',
-      subTitle: 'Danh sách từ vựng được phân loại theo cấp độ, loại từ, ...',
-      imgUrl: dictionaryIcon,
-    },
-    {
-      title: 'Từ vựng yêu thích của bạn',
-      imgUrl: favoriteIcon,
-      subTitle: 'Danh sách những từ vựng yêu thích mà bạn đã lưu',
-    },
+    // {
+    //   title: 'Từ điển trong EasyEnglish',
+    //   subTitle: 'Danh sách từ vựng được phân loại theo cấp độ, loại từ, ...',
+    //   imgUrl: dictionaryIcon,
+    // },
+    // {
+    //   title: 'Từ vựng yêu thích của bạn',
+    //   imgUrl: favoriteIcon,
+    //   subTitle: 'Danh sách những từ vựng yêu thích mà bạn đã lưu',
+    // },
     {
       title: 'Thêm từ vựng',
       imgUrl: editIcon,
